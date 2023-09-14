cb329dcc1baa0c3de4a63ea4e84232caeb7ec324
Rafal Lesnicki <lesnickirafal@gmail.com>
2023-09-14 14:24:42 +0200

    added ,part 2 kurs fix

--- src/zDomowe/zdomowe2/Bug.java
+++ src/zDomowe/zdomowe2/Bug.java
@@ -4,37 +4,47 @@ public class Bug {
 //    pola obiektu:
 //    opis błędu
 //    adres email osoby zgłaszajacej błąd
-//    priorytet błędu
-//    status błędu
+//    priorytet błędu 1-5
+//    status błędu (otwarty/zamknięty) - domyślnie zawsze otwarty
 
 //    konstruktor
 
 //    Metody
 //    pokaż wszystkie informacje na temat błędu
 //    pokaż osobę zgłaszającą błąd
+//    pokaż status błędu
 //    zwróć priorytet błędu
-
-    public Bug(String bugDescription, String reportingPersonEmail, String bugPriority, String bugStatus) {
-        this.bugDescription = bugDescription;
-        this.reportingPersonEmail = reportingPersonEmail;
-        this.bugPriority = bugPriority;
-        this.bugStatus = bugStatus;
-    }
 
     String bugDescription;
     String reportingPersonEmail;
-    String bugPriority;
-    String bugStatus;
+    int bugPriority;
+    boolean bugStatus;
 
-    void geAllInfo(){
+    public Bug(String bugDescription, String reportingPersonEmail, int bugPriority) {
+        this.bugDescription = bugDescription;
+        this.reportingPersonEmail = reportingPersonEmail;
+        this.bugPriority = bugPriority;
+        this.bugStatus = false;
+    }
+
+
+    void showAllInfo(){
         System.out.println(bugDescription + ", " + reportingPersonEmail + ", " + bugPriority + ", " + bugStatus);
     }
 
-    void getReportingPersonEmail(){
+    void showReportingPersonEmail(){
         System.out.println("Reporting person: " + reportingPersonEmail);
     }
 
-    String getBugPriority(){
+    void showBugStatus(){
+        if(bugStatus == false){
+            System.out.println("Bug status: Otwarty");
+        }else {
+            System.out.println("Bug status: Zamknięty");
+        }
+    }
+
+    int getBugPriority(){
         return bugPriority;
     }
 }
--- src/zDomowe/zdomowe2/BugMainApp.java
+++ src/zDomowe/zdomowe2/BugMainApp.java
@@ -3,15 +3,25 @@ package zDomowe.zdomowe2;
 public class BugMainApp {
     public static void main(String[] args) {
 
-        Bug loginBug =  new Bug("Can't login", "rafal@wp.pl", "medium", "closed");
-        loginBug.geAllInfo();
-        loginBug.getReportingPersonEmail();
-        loginBug.getBugPriority();
+        Bug loginBug =  new Bug("Can't login", "rafal@wp.pl", 3);
+        loginBug.showAllInfo();
+        loginBug.showReportingPersonEmail();
+        loginBug.showBugStatus();
+        int loginBugPriority = loginBug.getBugPriority();
+        System.out.println("Login Bug Priority: " +  loginBugPriority);
 
-        Bug emailBug =  new Bug("Can't add email", "piotr@wp.pl", "blocker", "open");
-        emailBug.geAllInfo();
-        emailBug.getReportingPersonEmail();
-        emailBug.getBugPriority();
+        loginBug.bugStatus =  true;
+        loginBug.showBugStatus();
+
+        Bug emailBug =  new Bug("Can't add email", "piotr@wp.pl", 5);
+        emailBug.showAllInfo();
+        emailBug.showReportingPersonEmail();
+        emailBug.showBugStatus();
+        int  emailBugPriority = emailBug.getBugPriority();
+        System.out.println("Email Bug Priority: " +  emailBugPriority);
+
+        emailBug.bugStatus =  true;
+        emailBug.showBugStatus();
 
     }
 }
